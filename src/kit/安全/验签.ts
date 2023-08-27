abca00ca70e1e1f0d470eea9e68c80e2b22d7a49
gxz <cnmmail163@163.com>
2023-08-27 16:22:33 +0800

    修复导入签

--- src/kit/安全/验签.ts
+++ src/kit/安全/验签.ts
@@ -6,7 +6,7 @@ export const 造验签 = () => crypto.subtle.generateKey(验签生, true, ['sign
 export const 导出验 = (验: CryptoKey) => crypto.subtle.exportKey('spki', 验)
 export const 导入验 = (验钥: BufferSource) => crypto.subtle.importKey('spki', 验钥, 验签生, false, ['verify'])
 export const 导出签 = (签: CryptoKey) => crypto.subtle.exportKey('pkcs8', 签)
-export const 导入签 = (签钥: BufferSource) => crypto.subtle.importKey('pkcs8', 签钥, 验签参, false, ['sign'])
+export const 导入签 = (签钥: BufferSource) => crypto.subtle.importKey('pkcs8', 签钥, 验签生, false, ['sign'])
 export const 搞验证 = (验: CryptoKey, 证: string, 文: BufferSource) => crypto.subtle.verify(验签参, 验, Buffer.from(证, 'base64'), 文).then(value => !value && Promise.reject(new 验证失败('身份被假冒或者数据被篡改')))
 export const 搞签证 = (签: CryptoKey, 文: BufferSource) => crypto.subtle.sign(验签参, 签, 文).then(Buffer.from).then(value => value.toString('base64'))
 
