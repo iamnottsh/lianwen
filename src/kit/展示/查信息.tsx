c879cdcf8b4807f3f4601f5de788c0eff212cc3b
gxz <cnmmail163@163.com>
2023-08-29 17:38:18 +0800

    修复查信息span

--- src/kit/展示/查信息.tsx
+++ src/kit/展示/查信息.tsx
@@ -7,9 +7,9 @@ export default function 查信息({
   _id: ObjectId
 }) {
   return (
-    <Box fontSize="smaller" display="flex" justifyContent="space-between">
-      <Box mr={1}>{_id.getTimestamp().toLocaleString()}</Box>
-      <Box>d4={_id.id.reduce((acc, cur) => acc + (cur >> 6) + (cur >> 4) + (cur >> 2) + cur & 3) + 1}</Box>
+    <Box component="span" fontSize="smaller" display="flex" justifyContent="space-between">
+      <Box component="span" mr={1}>{_id.getTimestamp().toLocaleString()}</Box>
+      <Box component="span">d4={_id.id.reduce((acc, cur) => acc + (cur >> 6) + (cur >> 4) + (cur >> 2) + cur & 3) + 1}</Box>
     </Box>
   )
 }
